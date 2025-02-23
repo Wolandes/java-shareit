c16fa2914aee59961c9af801599610a5ed726c11
Ranis Zyalalov <Jabelya@yandex.ru>
2025-02-23 20:48:53 +0300

    На ревью, исправление ошибок

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -14,7 +14,6 @@ import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
